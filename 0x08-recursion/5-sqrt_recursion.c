10460d8863b034937bae957cf069d826dd23a3e7
KenTanui <kentanuironoh@gmail.com>
2022-06-22 17:59:04 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,99 +1,38 @@
 #include "main.h"
 /**
- * _sqrt_recursion - Function
- *
- * Description: returns the natural square root of a number
- *
- * @n: parameter of type int
- *
- * Return: returns squat root of n, returns 1
- */
-int _sqrt_recursion(int n)
+ * sqtRecursive - computes square root recursively doing binary search
+ * @start: inital number
+ * @end: last number within the limit of number
+ * @m: given number
+ * Return: 1 if not found sqrroot, else sqrroot
+ **/
+int sqtRecursive(int start, int end, int m)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 1)
-	{
-		return (1);
-	}
-	else if (check_non_squareroot(n) == 0)
-	{
+	long mid;
 
-		return (-1);
-	}
-	else
+	if (end >= start)
 	{
-		return (_sqrt(n, n / 2));
+		mid = start + (end - start) / 2;
+		if (mid * mid == m)
+			return (mid);
+/* following binary search */
+		if (mid * mid > m)
+			return (sqtRecursive(start, mid - 1, m));
+		if (mid * mid < m)
+			return (sqtRecursive(mid + 1, end, m));
 	}
-}
-/**
- * _sqrt - Function
- *
- * Description: finds the square root of a number
- *
- * @root: parameter of type int, guessed root
- * @n: parameter of type int, get square root of
- *
- * Return: returns square root of num1
- */
-int _sqrt(int n, int root)
-{
-	if ((root * root) > n)
-	{
-		return (_sqrt(n, root / 2));
-	}
-	else if (root * root < n)
-	{
-		return (_sqrt(n, root + 1));
-	}
-	if (root * root == n)
-		return (root);
 	return (-1);
 }
 /**
- * check_non_squareroot - Function
- *
- * Description: checks if a number is a squareroot
- *
- * @num: number to check
- *
- *  Return: 0 or 1
- */
-int check_non_squareroot(int num)
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: given number
+ * Return: square root of n or -1
+ **/
+	int _sqrt_recursion(int n)
 {
-	if ((num % 10 ==  0
-	|| num % 10 == 1
-	|| num % 10 == 4
-	|| num % 10 == 5
-	|| num % 10 == 6
-	|| num % 10 == 9)
-	&& (sum_num(num) == 1
-	|| sum_num(num) == 4
-	|| sum_num(num) == 7
-	|| sum_num(num) == 9))
-	{
-		return (1);
-	}
-	else
-
-	return (0);
-}
-/**
- * sum_num - Function
- *
- * Description: calculates the sum of the digits in num
- *
- * @num: parameter of type int
- *
- * Return: returns the sum
- */
-int sum_num(int num)
-{
-	if (num < 10)
-	{
-		return (num);
-	}
-	return (sum_num(num % 10 + sum_num(num / 10)));
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqtRecursive(2, n, n));
 }
