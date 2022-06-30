30e93ef55ca04ed14912a0f5a0ddfcc15ecc94d9
KenTanui <kentanuironoh@gmail.com>
2022-06-30 13:17:59 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+s (20 sloc)  324 Bytes
+
+#include "main.h"
+/**
+ * malloc_checked - Function
+ *
+ * Description: allocates memory using malloc
+ *
+ * @b: parameter of type unsigned int
+ *
+ * Return: returns status value 98 if fails.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *alloc;
+
+	alloc = malloc(b);
+	if (alloc == NULL)
+	{
+		exit(98);
+	}
+	return (alloc);
+}
