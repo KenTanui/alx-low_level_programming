335efd59e24c084352e76f2fb72e07b384d6ea99
kentanui <kentanuironoh@gmail.com>
2022-09-23 16:49:31 -0700

    1-main.c

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: the string to hash
+ * Return: the calculated hash
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int f;
+
+	hash = 5381;
+	while ((f = *str++))
+		hash = ((hash << 5) + hash) + f;
+
+	return (hash);
+}
